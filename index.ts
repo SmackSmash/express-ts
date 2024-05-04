b3f6b4d43e614dd1e10d7bc375b073f63193245f
Dan Moffat <danmoffat@gmail.com>
2024-05-04 18:53:12 +0100

    Database connection and another TS hack...

--- index.ts
+++ index.ts
@@ -1,6 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
 require('dotenv').config();
 
 const app = express();
 
-app.listen(process.env.PORT || 5000);
+connectDB();
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
+
+export {};
