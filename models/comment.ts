6b26c40fae4d03381598da1c62cebe73da4adf85
Dan Moffat <danmoffat@gmail.com>
2024-05-04 21:27:31 +0100

    Finally got the DB connected

--- models/comment.ts
+++ models/comment.ts
@@ -1,14 +1,12 @@
 import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
+// const ObjectId = Schema.ObjectId;
 
 const CommentSchema = new Schema({
   name: String,
   email: String,
-  movie_id: ObjectId,
-  text: String,
-  date: Date
+  text: String
 });
 
 export default mongoose.model('comments', CommentSchema);
--- routes/comments.ts
+++ routes/comments.ts
@@ -1,8 +1,11 @@
 import express, { type Request, type Response } from 'express';
+import Comment from '../models/comment';
 const router = express.Router();
 
-router.get('/', (req: Request, res: Response) => {
-  res.send({ hello: 'world' });
+router.get('/', async (req: Request, res: Response) => {
+  const comments = await Comment.find();
+  console.log(comments);
+  res.send(comments);
 });
 
 export default router;
